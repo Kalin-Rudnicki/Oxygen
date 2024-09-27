7b4720d120b3e077a5f386ef421a2db1597a5fcb
Kalin-Rudnicki <kalin.r.rudnicki@gmail.com>
2024-09-27 16:45:50 -0600

    Check for fix todo in CI pipeline

---
+++ check-for-fix.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if git grep "FIX-PRE-MERGE" -- ":!check-for-fix.sh"; then
+  if [[ "$ALLOW_FIX_PRE_MERGE" != "true" ]]; then
+    echo "Detected 'FIX-PRE-MERGE'"
+    exit 1
+  else
+    echo "Detected 'FIX-PRE-MERGE', allowing commit anyway"
+  fi
+fi
