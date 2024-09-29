67e6e3de53027e496aa08ae6bc4b3cf667922b5c
Kalin-Rudnicki <kalin.r.rudnicki@gmail.com>
2024-09-29 11:10:13 -0600

    Added missing zio exports and StringCodec.uuid

--- modules/zio/src/main/scala/oxygen/predef/zio.scala
+++ modules/zio/src/main/scala/oxygen/predef/zio.scala
@@ -1,8 +1,11 @@
 package oxygen.predef
 
 object zio {
-  export _root_.zio.{FiberRef, Ref, Schedule, Scope}
-  export _root_.zio.{IO, RIO, Task, URIO, ZIO}
+  export _root_.zio.{Chunk, FiberRef, Ref, Schedule, Scope}
+  export _root_.zio.{Clock, Random, System}
+  export _root_.zio.{durationInt, durationLong, DurationOps, DurationSyntax}
+  export _root_.zio.{IO, RIO, Task, UIO, URIO, ZIO}
   export _root_.zio.{Layer, RLayer, TaskLayer, URLayer, ZLayer}
+  export oxygen.zio.{FiberRefModification, FiberRefModificationR, OxygenEnv, ZIOAspectAtLeastR, ZIOAspectPoly}
   export oxygen.zio.logger.{Logger, LogLevel}
 }
