c54613b2a067b5d99c0c833cf183938f427a2288
Kalin-Rudnicki <kalin.r.rudnicki@gmail.com>
2025-06-29 11:02:30 -0600

    Add type information to `oxygen.quoted.Util.showExpr`

--- modules/quoted/src/main/scala/oxygen/quoted/Util.scala
+++ modules/quoted/src/main/scala/oxygen/quoted/Util.scala
@@ -4,11 +4,28 @@ import scala.quoted.*
 
 object Util {
 
-  private def showExprImpl[A: Type](a: Expr[A])(using Quotes): Expr[A] = {
-    report.info(a.showAnsiCode)
-    a
+  private def showExprImpl[A: Type](expr: Expr[A])(using Quotes): Expr[A] = {
+    val tpeFromType: TypeRepr = TypeRepr.of[A]
+    val tpeFromTerm: TypeRepr = expr.toTerm.tpe
+    report.info(
+      s"""=====| TypeRepr.of[A] |=====
+         |                   type: ${tpeFromType.showAnsiCode}
+         |             type.widen: ${tpeFromType.widen.showAnsiCode}
+         |           type.dealias: ${tpeFromType.dealias.showAnsiCode}
+         |type.dealiasKeepOpaques: ${tpeFromType.dealiasKeepOpaques.showAnsiCode}
+         |
+         |=====| expr.toTerm.tpe |=====
+         |                   type: ${tpeFromTerm.showAnsiCode}
+         |             type.widen: ${tpeFromTerm.widen.showAnsiCode}
+         |           type.dealias: ${tpeFromTerm.dealias.showAnsiCode}
+         |type.dealiasKeepOpaques: ${tpeFromTerm.dealiasKeepOpaques.showAnsiCode}
+         |
+         |=====| Expr |=====
+         |${expr.showAnsiCode}""".stripMargin,
+      expr,
+    )
+    expr
   }
-
   inline def showExpr[A](inline a: A): A = ${ showExprImpl[A]('a) }
 
 }
