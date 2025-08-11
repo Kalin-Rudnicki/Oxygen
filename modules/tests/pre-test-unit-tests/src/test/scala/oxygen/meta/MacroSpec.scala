6bf806a19944a422553d764a1c84268a9719b598
Kalin-Rudnicki <kalin.r.rudnicki@gmail.com>
2025-08-11 10:27:25 -0600

    Add EnumGeneric helper to derive enum values in a variety of cases

--- modules/tests/pre-test-unit-tests/src/test/scala/oxygen/meta/MacroSpec.scala
+++ modules/tests/pre-test-unit-tests/src/test/scala/oxygen/meta/MacroSpec.scala
@@ -12,6 +12,21 @@ object MacroSpec extends OxygenSpecDefault {
 
   final case class Class1(a: Int = 5, b: Option[String] = None, c: Boolean)
 
+  sealed trait Sum1
+  object Sum1 {
+    case object Case1 extends Sum1
+    case object Case2 extends Sum1
+    case object Case3 extends Sum1
+  }
+
+  sealed trait Sum2
+  object Sum2 {
+    case object Case1 extends Sum2
+    case object Case2 extends Sum2
+    case object Case3 extends Sum2
+    final case class Other(s: String) extends Sum2
+  }
+
   override def testSpec: TestSpec =
     suite("MacroSpec")(
       suite("seq")(
@@ -39,6 +54,20 @@ object MacroSpec extends OxygenSpecDefault {
       suite("default args")(
         test("Class1") {
           assertTrue(Macros.defaultArgs[Class1] == "[a = 5, b = None]")
+        },
+      ),
+      suite("enum values")(
+        test("Sum1") {
+          assertTrue(
+            K0.SumGeneric.EnumGeneric.deriveEnum.strictEnum.values[Sum1].toSet ==
+              Set(Sum1.Case1, Sum1.Case2, Sum1.Case3),
+          )
+        },
+        test("Sum2") {
+          assertTrue(
+            K0.SumGeneric.EnumGeneric.deriveEnum.ignoreSingleCaseClass.values[Sum2].toSet ==
+              Set(Sum2.Case1, Sum2.Case2, Sum2.Case3),
+          )
         },
       ),
     )
