2a9011b3bdc2684c882d11739eecc02a067f02b6
Kalin-Rudnicki <kalin.r.rudnicki@gmail.com>
2025-10-18 14:40:50 -0600

    Added GlobalMemoizedEffect

--- modules/general/zio/src/main/scala/oxygen/predef/zio.scala
+++ modules/general/zio/src/main/scala/oxygen/predef/zio.scala
@@ -6,7 +6,7 @@ object zio {
   export _root_.zio.{durationInt, durationLong, DurationOps, DurationSyntax}
   export _root_.zio.{IO, RIO, Task, UIO, URIO, ZIO}
   export _root_.zio.{Layer, RLayer, TaskLayer, ULayer, URLayer, ZLayer}
-  export oxygen.zio.{ZIOAspectAtLeastR, ZIOAspectPoly}
+  export oxygen.zio.{GlobalLayer, GlobalMemoizedEffect, ZIOAspectAtLeastR, ZIOAspectPoly}
   export oxygen.zio.instances.given
   export oxygen.zio.logging.LogLevels
   export oxygen.zio.syntax.log.*
--- modules/general/zio/src/main/scala/oxygen/zio/GlobalLayer.scala
+++ modules/general/zio/src/main/scala/oxygen/zio/GlobalLayer.scala
@@ -1,25 +1,45 @@
 package oxygen.zio
 
-import oxygen.predef.core.*
 import zio.*
 
 abstract class GlobalLayer[E, A] {
 
-  private val cache: Ref.Synchronized[Option[ZEnvironment[A]]] =
-    Unsafe.unsafely { Ref.Synchronized.unsafe.make(None) }
-
   protected val underlyingLayer: Layer[E, A]
 
-  private lazy val makeEnvironment: IO[E, ZEnvironment[A]] =
-    underlyingLayer.build(Scope.global)
-
-  private lazy val getOrMakeEnvironment: IO[E, ZEnvironment[A]] =
-    cache.modifyZIO {
-      case current @ Some(env) => ZIO.succeed((env, current))
-      case None                => makeEnvironment.map { env => (env, env.some) }
-    }
+  private lazy val effect: IO[E, ZEnvironment[A]] =
+    GlobalMemoizedEffect.cacheVal { underlyingLayer.build(Scope.global) }
 
   final val layer: Layer[E, A] =
-    ZLayer.fromZIOEnvironment { getOrMakeEnvironment }
+    ZLayer.fromZIOEnvironment { effect }
+
+}
+object GlobalLayer {
+
+  def make[E, A](underlyingLayer: Layer[E, A]): GlobalLayer[E, A] = {
+    val tmp = underlyingLayer
+    new GlobalLayer[E, A] { override protected val underlyingLayer: Layer[E, A] = tmp }
+  }
+
+  /**
+    * Note, this should only be called when assigning the result to a global value.
+    * If not, the global cache ref will not be the same, and this does nothing.
+    *
+    * --- good ---
+    * val res: ULayer[Int] = GlobalLayer.cacheVal { ZLayer { Random.nextInt } }
+    *
+    * --- bad ---
+    * def res: ULayer[Int] = GlobalLayer.cacheVal { ZLayer { Random.nextInt } }
+    *
+    * --- bad ---
+    * val res: ULayer[Int] =
+    *   for {
+    *     _ <- ZLayer { ZIO.logInfo("logging") }
+    *     res <- GlobalLayer.cacheVal { ZLayer { Random.nextInt } }
+    *   } yield res
+    */
+  def cacheVal[E, A](underlyingLayer: Layer[E, A]): Layer[E, A] = {
+    val layer: GlobalLayer[E, A] = GlobalLayer.make(underlyingLayer)
+    layer.layer
+  }
 
 }
