74c611c6a107f750345f516c44d1c2620f982d4f
Kalin-Rudnicki <kalin.r.rudnicki@gmail.com>
2025-10-25 11:38:41 -0600

    WIP : adding empty Element/Span

---
+++ modules/slyce/core/src/main/scala/oxygen/slyce/core/Element.scala
@@ -0,0 +1,19 @@
+package oxygen.slyce.core
+
+sealed trait Element {
+
+  val elementName: String
+
+}
+
+trait Token extends Element {
+
+  // FIX-PRE-MERGE (KR) :
+
+}
+
+trait Node extends Element {
+
+  // FIX-PRE-MERGE (KR) :
+
+}
---
+++ modules/slyce/core/src/main/scala/oxygen/slyce/core/Span.scala
@@ -0,0 +1,13 @@
+package oxygen.slyce.core
+
+sealed trait Span
+object Span {
+
+  // FIX-PRE-MERGE (KR) :
+
+  // Range
+  // ZeroWidth
+  // EOF
+  // Unknown (needed? covered by ZeroWidth?)
+
+}
