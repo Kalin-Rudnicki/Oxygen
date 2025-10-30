45c33d7dcc1db16a4b867662241cd61d53692603
Kalin-Rudnicki <kalin.r.rudnicki@gmail.com>
2025-10-30 17:05:57 -0600

    Added missing items to RequestPathCodec

--- modules/http/zio/src/main/scala/oxygen/http/core/RequestPathCodec.scala
+++ modules/http/zio/src/main/scala/oxygen/http/core/RequestPathCodec.scala
@@ -2,13 +2,15 @@ package oxygen.http.core
 
 import oxygen.http.core.partial.PartialPathCodec
 import oxygen.http.schema.partial.{PartiallyAppliedPathSchema, RequestSchemaAggregator}
+import oxygen.meta.*
 import oxygen.predef.core.*
 import scala.annotation.tailrec
+import scala.quoted.*
 import scala.reflect.TypeTest
 
 sealed trait RequestPathCodec[A] {
 
-  val partiallyAppliedPathSchemas: NonEmptyList[Growable[PartiallyAppliedPathSchema]]
+  lazy val partiallyAppliedPathSchemas: NonEmptyList[Growable[PartiallyAppliedPathSchema]]
 
   final lazy val schemaAggregator: RequestSchemaAggregator = RequestSchemaAggregator.paths(partiallyAppliedPathSchemas)
 
@@ -18,17 +20,33 @@ sealed trait RequestPathCodec[A] {
   final def <||[B >: A](that: RequestPathCodec[B])(using ct: TypeTest[B, A]): RequestPathCodec[B] = RequestPathCodec.OrElse(this, that, ct)
   final def ||>[B <: A](that: RequestPathCodec[B])(using ct: TypeTest[A, B]): RequestPathCodec[A] = RequestPathCodec.OrElse(that, this, ct)
 
+  final def /[B](that: RequestPathCodec[B])(using zip: Zip[A, B]): RequestPathCodec[zip.Out] =
+    RequestPathCodec.AndThen(this, that, zip)
+
   final def transform[B](ab: A => B, ba: B => A): RequestPathCodec[B] = RequestPathCodec.Transform(this, ab, ba)
   final def transformDecode[B](ab: A => Option[B], ba: B => A): RequestPathCodec[B] = RequestPathCodec.TransformDecode(this, ab, ba)
 
+  inline final def autoTransform[B]: RequestPathCodec[B] = {
+    val (ab, ba) = K0.ProductGeneric.deriveTransform[A, B]
+    transform(ab, ba)
+  }
+
 }
 object RequestPathCodec {
+
+  def const(path: String): RequestPathCodec[Unit] = ConstSingle(path)
+
+  def applied[A: PartialPathCodec as c](name: String, doc: Option[String] = None): RequestPathCodec[A] = ApplyPartial(c, name, doc)
+  def plain[A: PartialPathCodec.Plain as c](name: String, doc: Option[String] = None): RequestPathCodec[A] = ApplyPartial(c, name, doc)
+  def json[A: PartialPathCodec.Json as c](name: String, doc: Option[String] = None): RequestPathCodec[A] = ApplyPartial(c, name, doc)
+
+  given stringToPathCodec: Conversion[String, RequestPathCodec[Unit]] = const(_)
 
   final case class ConstSingle(path: String) extends RequestPathCodec[Unit] {
 
     private val encoded: Growable[String] = Growable.single(path)
 
-    override val partiallyAppliedPathSchemas: NonEmptyList[Growable[PartiallyAppliedPathSchema]] =
+    override lazy val partiallyAppliedPathSchemas: NonEmptyList[Growable[PartiallyAppliedPathSchema]] =
       NonEmptyList.one(Growable.single(PartiallyAppliedPathSchema.Const(path)))
 
     override def decode(paths: List[String]): Option[(Unit, List[String])] = paths match
@@ -43,7 +61,7 @@ object RequestPathCodec {
 
     private val encoded: Growable[String] = Growable.many(paths)
 
-    override val partiallyAppliedPathSchemas: NonEmptyList[Growable[PartiallyAppliedPathSchema]] =
+    override lazy val partiallyAppliedPathSchemas: NonEmptyList[Growable[PartiallyAppliedPathSchema]] =
       NonEmptyList.one(Growable.many(paths).map(PartiallyAppliedPathSchema.Const(_)))
 
     @tailrec
@@ -68,7 +86,7 @@ object RequestPathCodec {
 
   final case class ApplyPartial[A](partial: PartialPathCodec[A], name: String, doc: Option[String]) extends RequestPathCodec[A] {
 
-    override val partiallyAppliedPathSchemas: NonEmptyList[Growable[PartiallyAppliedPathSchema]] =
+    override lazy val partiallyAppliedPathSchemas: NonEmptyList[Growable[PartiallyAppliedPathSchema]] =
       NonEmptyList.one(Growable.single(PartiallyAppliedPathSchema.Param(name, partial.partialPathSchema.tpe, partial.partialPathSchema.schema, doc)))
 
     override def decode(paths: List[String]): Option[(A, List[String])] = partial.decode(paths)
@@ -78,7 +96,7 @@ object RequestPathCodec {
 
   final case class AndThen[A, B, C](a: RequestPathCodec[A], b: RequestPathCodec[B], zip: Zip.Out[A, B, C]) extends RequestPathCodec[C] {
 
-    override val partiallyAppliedPathSchemas: NonEmptyList[Growable[PartiallyAppliedPathSchema]] =
+    override lazy val partiallyAppliedPathSchemas: NonEmptyList[Growable[PartiallyAppliedPathSchema]] =
       (a.partiallyAppliedPathSchemas, b.partiallyAppliedPathSchemas) match {
         case (NonEmptyList(aPaths, Nil), NonEmptyList(bPaths, Nil)) => NonEmptyList.one(aPaths ++ bPaths)
         case _                                                      =>
@@ -103,7 +121,7 @@ object RequestPathCodec {
 
   final case class OrElse[A, B >: A](a: RequestPathCodec[A], b: RequestPathCodec[B], tt: TypeTest[B, A]) extends RequestPathCodec[B] {
 
-    override val partiallyAppliedPathSchemas: NonEmptyList[Growable[PartiallyAppliedPathSchema]] =
+    override lazy val partiallyAppliedPathSchemas: NonEmptyList[Growable[PartiallyAppliedPathSchema]] =
       a.partiallyAppliedPathSchemas ++ b.partiallyAppliedPathSchemas
 
     override def decode(paths: List[String]): Option[(B, List[String])] =
@@ -117,7 +135,7 @@ object RequestPathCodec {
 
   final case class Transform[A, B](a: RequestPathCodec[A], ab: A => B, ba: B => A) extends RequestPathCodec[B] {
 
-    override val partiallyAppliedPathSchemas: NonEmptyList[Growable[PartiallyAppliedPathSchema]] =
+    override lazy val partiallyAppliedPathSchemas: NonEmptyList[Growable[PartiallyAppliedPathSchema]] =
       a.partiallyAppliedPathSchemas
 
     override def decode(paths: List[String]): Option[(B, List[String])] =
@@ -130,7 +148,7 @@ object RequestPathCodec {
 
   final case class TransformDecode[A, B](a: RequestPathCodec[A], ab: A => Option[B], ba: B => A) extends RequestPathCodec[B] {
 
-    override val partiallyAppliedPathSchemas: NonEmptyList[Growable[PartiallyAppliedPathSchema]] =
+    override lazy val partiallyAppliedPathSchemas: NonEmptyList[Growable[PartiallyAppliedPathSchema]] =
       a.partiallyAppliedPathSchemas
 
     override def decode(paths: List[String]): Option[(B, List[String])] =
@@ -141,6 +159,60 @@ object RequestPathCodec {
 
     override def encode(value: B): Growable[String] =
       a.encode(ba(value))
+
+  }
+
+  trait FromDecodeCases[A] extends RequestPathCodec[A] {
+
+    val underlying: NonEmptyList[RequestPathCodec[? <: A]]
+
+    override final lazy val partiallyAppliedPathSchemas: NonEmptyList[Growable[PartiallyAppliedPathSchema]] =
+      underlying.flatMap(_.partiallyAppliedPathSchemas)
+
+    @tailrec
+    private def decodeRec(paths: List[String], codecs: List[RequestPathCodec[? <: A]]): Option[(A, List[String])] =
+      codecs match {
+        case codec :: rest =>
+          codec.decode(paths) match {
+            case some @ Some(_) => some
+            case None           => decodeRec(paths, rest)
+          }
+        case Nil =>
+          None
+      }
+
+    override final def decode(paths: List[String]): Option[(A, List[String])] =
+      decodeRec(paths, underlying.toList)
+
+  }
+  object FromDecodeCases {
+
+    private def derivedImpl[A: Type](using Quotes): Expr[FromDecodeCases[A]] = {
+      val gen: K0.SumGeneric[A] = K0.SumGeneric.of[A]
+      gen.cacheVals.summonTypeClasses[RequestPathCodec]().defineAndUse { instances =>
+        def schemas: List[Expr[RequestPathCodec[? <: A]]] =
+          gen.mapChildren.mapExpr[RequestPathCodec[? <: A]] { [b <: A] => (_, _) ?=> (kase: gen.Case[b]) => kase.getExpr(instances) }.to[List]
+
+        def encodeImpl(value: Expr[A]): Expr[Growable[String]] =
+          gen.matcher.instance(value) { [b <: A] => (_, _) ?=> (kase: gen.Case[b]) =>
+            kase.caseExtractor.withRHS { value => '{ ${ kase.getExpr(instances) }.encode($value) } }
+          }
+
+        '{
+          new FromDecodeCases[A] {
+
+            override val underlying: NonEmptyList[RequestPathCodec[? <: A]] =
+              NonEmptyList.unsafeFromList { ${ schemas.seqToExpr } }
+
+            override def encode(value: A): Growable[String] = ${ encodeImpl('value) }
+
+          }
+        }
+      }
+    }
+
+    // currently requires that you have derived an implicit instance for each case already
+    inline def derived[A]: FromDecodeCases[A] = ${ derivedImpl[A] }
 
   }
 
