bbba5ddadab4e0ee22542d0cc81d6dd2af611c26
Kalin-Rudnicki <kalin.r.rudnicki@gmail.com>
2025-10-31 08:28:52 -0600

    Added missing instances of json field codecs

--- modules/general/json/src/main/scala/oxygen/json/JsonFieldEncoder.scala
+++ modules/general/json/src/main/scala/oxygen/json/JsonFieldEncoder.scala
@@ -1,11 +1,22 @@
 package oxygen.json
 
+import oxygen.predef.core.*
+
 final case class JsonFieldEncoder[A](encode: A => String) {
   def contramap[B](f: B => A): JsonFieldEncoder[B] = JsonFieldEncoder(b => encode(f(b)))
 }
-object JsonFieldEncoder {
+object JsonFieldEncoder extends JsonFieldEncoderLowPriority.LowPriority1 {
 
-  // TODO (KR) :
   given string: JsonFieldEncoder[String] = JsonFieldEncoder(identity)
+
+}
+
+object JsonFieldEncoderLowPriority {
+
+  trait LowPriority1 {
+
+    given fromStringEncoder: [A: StringEncoder as enc] => JsonFieldEncoder[A] = JsonFieldEncoder.string.contramap(enc.encode)
+
+  }
 
 }
