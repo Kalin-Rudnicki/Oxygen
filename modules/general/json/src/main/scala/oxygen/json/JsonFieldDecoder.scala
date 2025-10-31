6c196065739d79ffa2b69f557b5f86fce35278e3
Kalin-Rudnicki <kalin.r.rudnicki@gmail.com>
2025-10-31 08:28:52 -0600

    Added missing instances of json field codecs

--- modules/general/json/src/main/scala/oxygen/json/JsonFieldDecoder.scala
+++ modules/general/json/src/main/scala/oxygen/json/JsonFieldDecoder.scala
@@ -6,8 +6,18 @@ final case class JsonFieldDecoder[A](decode: String => Either[String, A]) {
   def map[B](f: A => B): JsonFieldDecoder[B] = JsonFieldDecoder(decode(_).map(f))
   def mapOrFail[B](f: A => Either[String, B]): JsonFieldDecoder[B] = JsonFieldDecoder(decode(_).flatMap(f))
 }
-object JsonFieldDecoder {
+object JsonFieldDecoder extends JsonFieldDecoderLowPriority.LowPriority1 {
 
   given string: JsonFieldDecoder[String] = JsonFieldDecoder(_.asRight)
+
+}
+
+object JsonFieldDecoderLowPriority {
+
+  trait LowPriority1 {
+
+    given fromStringDecoder: [A: StringDecoder as dec] => JsonFieldDecoder[A] = JsonFieldDecoder.string.mapOrFail(dec.decode)
+
+  }
 
 }
