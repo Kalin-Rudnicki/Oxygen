406a94ad48711f90c51ca7e3911ebdbcc24b63d3
Kalin-Rudnicki <kalin.r.rudnicki@gmail.com>
2025-12-17 10:00:00 -0700

    Tweaked middlewares

--- modules/http/zio/.jvm/src/main/scala/oxygen/http/server/RequestMiddleware.scala
+++ modules/http/zio/.jvm/src/main/scala/oxygen/http/server/RequestMiddleware.scala
@@ -2,6 +2,7 @@ package oxygen.http.server
 
 import oxygen.http.model.internal.*
 import zio.*
+import zio.http.Path
 
 trait RequestMiddleware {
 
@@ -9,15 +10,51 @@ trait RequestMiddleware {
 
   final def >>>(that: RequestMiddleware): RequestMiddleware =
     (this, that) match
-      case (RequestMiddleware.Empty, _) => that
-      case (_, RequestMiddleware.Empty) => this
-      case _                            => this.apply(_).flatMap(that.apply)
+      case (RequestMiddleware.Empty, _)                                             => that
+      case (_, RequestMiddleware.Empty)                                             => this
+      case (self: RequestMiddleware.Effectless, that: RequestMiddleware.Effectless) => RequestMiddleware.ThenEffectless(self, that)
+      case _                                                                        => RequestMiddleware.ThenEffectful(this, that)
 
 }
 object RequestMiddleware {
 
-  case object Empty extends RequestMiddleware {
-    override def apply(request: ReceivedRequest): UIO[ReceivedRequest] = ZIO.succeed(request)
+  //////////////////////////////////////////////////////////////////////////////////////////////////////
+  //      Default Instances
+  //////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  final case class AddPrefix(path: Path) extends RequestMiddleware.Effectless {
+    override def applyEffectless(request: ReceivedRequest): ReceivedRequest = {
+      val newPath: Path = path ++ request.url.path
+      request.copy(url = request.url.copy(path = newPath))
+    }
+  }
+
+  final case class DropPrefix(path: Path) extends RequestMiddleware.Effectless {
+    override def applyEffectless(request: ReceivedRequest): ReceivedRequest = {
+      val newPath: Path = request.url.path.unnest(path)
+      request.copy(url = request.url.copy(path = newPath))
+    }
+  }
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////
+  //      Helpers
+  //////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  trait Effectless extends RequestMiddleware {
+    def applyEffectless(request: ReceivedRequest): ReceivedRequest
+    override final def apply(request: ReceivedRequest): UIO[ReceivedRequest] = ZIO.succeed(applyEffectless(request))
+  }
+
+  case object Empty extends RequestMiddleware.Effectless {
+    override def applyEffectless(request: ReceivedRequest): ReceivedRequest = request
+  }
+
+  final case class ThenEffectless(a: RequestMiddleware.Effectless, b: RequestMiddleware.Effectless) extends RequestMiddleware.Effectless {
+    override def applyEffectless(request: ReceivedRequest): ReceivedRequest = b.applyEffectless(a.applyEffectless(request))
+  }
+
+  final case class ThenEffectful(a: RequestMiddleware, b: RequestMiddleware) extends RequestMiddleware {
+    override def apply(request: ReceivedRequest): URIO[Scope, ReceivedRequest] = a.apply(request).flatMap(b.apply)
   }
 
 }
--- modules/http/zio/.jvm/src/main/scala/oxygen/http/server/ResponseMiddleware.scala
+++ modules/http/zio/.jvm/src/main/scala/oxygen/http/server/ResponseMiddleware.scala
@@ -9,15 +9,39 @@ trait ResponseMiddleware {
 
   final def >>>(that: ResponseMiddleware): ResponseMiddleware =
     (this, that) match
-      case (ResponseMiddleware.Empty, _) => that
-      case (_, ResponseMiddleware.Empty) => this
-      case _                             => this.apply(_).flatMap(that.apply)
+      case (ResponseMiddleware.Empty, _)                                              => that
+      case (_, ResponseMiddleware.Empty)                                              => this
+      case (self: ResponseMiddleware.Effectless, that: ResponseMiddleware.Effectless) => ResponseMiddleware.ThenEffectless(self, that)
+      case _                                                                          => ResponseMiddleware.ThenEffectful(this, that)
 
 }
 object ResponseMiddleware {
 
-  case object Empty extends ResponseMiddleware {
-    override def apply(response: Response): UIO[Response] = ZIO.succeed(response)
+  //////////////////////////////////////////////////////////////////////////////////////////////////////
+  //      Default Instances
+  //////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  // TODO (KR) :
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////
+  //      Helpers
+  //////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  trait Effectless extends ResponseMiddleware {
+    def applyEffectless(response: Response): Response
+    override final def apply(response: Response): URIO[Scope, Response] = ZIO.succeed(response)
+  }
+
+  case object Empty extends ResponseMiddleware.Effectless {
+    override def applyEffectless(response: Response): Response = response
+  }
+
+  final case class ThenEffectless(a: ResponseMiddleware.Effectless, b: ResponseMiddleware.Effectless) extends ResponseMiddleware.Effectless {
+    override def applyEffectless(response: Response): Response = b.applyEffectless(a.applyEffectless(response))
+  }
+
+  final case class ThenEffectful(a: ResponseMiddleware, b: ResponseMiddleware) extends ResponseMiddleware {
+    override def apply(response: Response): URIO[Scope, Response] = a.apply(response).flatMap(b.apply)
   }
 
 }
