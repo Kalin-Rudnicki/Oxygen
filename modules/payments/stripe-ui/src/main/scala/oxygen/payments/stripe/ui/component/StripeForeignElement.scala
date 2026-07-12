5944a15ffdc3ed0685b478b7d0e060728c60194a
Kalin Rudnicki <kalin.r.rudnicki@gmail.com>
2026-07-12 08:29:29 -0600

    WIP : savepoint

--- modules/payments/stripe-ui/src/main/scala/oxygen/payments/stripe/ui/component/StripeForeignElement.scala
+++ modules/payments/stripe-ui/src/main/scala/oxygen/payments/stripe/ui/component/StripeForeignElement.scala
@@ -5,18 +5,21 @@ import oxygen.payments.stripe.ui.facades as F
 import oxygen.ui.web.internal.*
 import zio.*
 
-final class StripeForeignElement private (stripe: F.StripeElement)(using ForeignElement.Register) extends ForeignElement {
+final class StripeForeignElement private (
+    private[stripe] val elements: F.StripeElements,
+    private[stripe] val element: F.StripeElement,
+)(using ForeignElement.Register) extends ForeignElement {
 
   override val name: String = "Stripe Element"
 
-  override protected def mountInternal(stableId: String, element: Element): Unit = stripe.mount(element)
-  override protected def unmountInternal(): Unit = stripe.unmount()
-  override protected def destroyInternal(): Unit = stripe.destroy()
+  override protected def mountInternal(stableId: String, element: Element): Unit = this.element.mount(element)
+  override protected def unmountInternal(): Unit = this.element.unmount()
+  override protected def destroyInternal(): Unit = this.element.destroy()
 
 }
 object StripeForeignElement {
 
-  def register(stripe: F.StripeElement): URIO[Scope, StripeForeignElement] =
-    ForeignElement.register { new StripeForeignElement(stripe) }
+  def register(elements: F.StripeElements, element: F.StripeElement): URIO[Scope, StripeForeignElement] =
+    ForeignElement.register { new StripeForeignElement(elements, element) }
 
 }
