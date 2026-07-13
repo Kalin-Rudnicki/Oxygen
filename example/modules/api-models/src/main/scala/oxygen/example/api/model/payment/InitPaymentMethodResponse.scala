6c1c6b6c8dff29ffcc51e3625362f2df5fc63a80
Kalin Rudnicki <kalin.r.rudnicki@gmail.com>
2026-07-12 19:36:28 -0600

    WIP : savepoint - making example happen

---
+++ example/modules/api-models/src/main/scala/oxygen/example/api/model/payment/InitPaymentMethodResponse.scala
@@ -0,0 +1,7 @@
+package oxygen.example.api.model.payment
+
+import oxygen.schema.JsonSchema
+
+final case class InitPaymentMethodResponse(
+                                            // FIX-PRE-MERGE (KR) :
+) derives JsonSchema
--- example/modules/api-models/src/main/scala/oxygen/example/api/model/payment/PaymentMethod.scala
+++ example/modules/api-models/src/main/scala/oxygen/example/api/model/payment/PaymentMethod.scala
@@ -1,5 +1,12 @@
 package oxygen.example.api.model.payment
 
-final case class PaymentMethod (
-    id:
-                               )
+import java.time.Instant
+import oxygen.example.core.model.*
+import oxygen.schema.JsonSchema
+
+final case class PaymentMethod(
+    id: PaymentMethodId,
+    name: Option[String],
+    // FIX-PRE-MERGE (KR) :
+    createdAt: Instant,
+) derives JsonSchema
