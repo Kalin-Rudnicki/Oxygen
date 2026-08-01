c47586995dababc764bb14d08e5edd38a92d009d
Kalin Rudnicki <kalin.r.rudnicki@gmail.com>
2026-08-01 05:30:58 -0600

    WIP : savepoint

--- modules/general/zio/src/main/scala/oxygen/zio/system/UnimplementedCommandService.scala
+++ modules/general/zio/src/main/scala/oxygen/zio/system/UnimplementedCommandService.scala
@@ -5,7 +5,27 @@ import zio.*
 
 object UnimplementedCommandService extends CommandService {
 
-  override def executeToOutputs(command: Command2): IO[CommandError, (stdOut: String, stdErr: String, exitCode: RuntimeFlags)] =
+  override def executeSync(
+      command: BuiltCommand,
+      stdIn: CommandInputSource,
+      trim: Boolean,
+  )(using Trace): IO[CommandError, (stdOut: String, stdErr: String, exitCode: Int)] =
+    ??? // FIX-PRE-MERGE (KR) :
+
+  override def executeSyncStreamErr(
+      command: BuiltCommand,
+      stdIn: CommandInputSource,
+      stdErr: CommandOutputSource,
+      trim: Boolean,
+  )(using Trace): IO[CommandError, (stdOut: String, exitCode: Int)] =
+    ??? // FIX-PRE-MERGE (KR) :
+
+  override def executeCode(
+      command: BuiltCommand,
+      stdIn: CommandInputSource,
+      stdOut: CommandOutputSource,
+      stdErr: CommandOutputSource,
+  )(using Trace): IO[CommandError, Int] =
     ??? // FIX-PRE-MERGE (KR) :
 
 }
